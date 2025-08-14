11f3f0171feb4b2699daf88ff2044585bee0817f
Ashan Kobewatta <ashanpulasthi@gmail.com>
2025-08-14 12:38:41 -0700

    Update update_cache.py

--- backend/update_cache.py
+++ backend/update_cache.py
@@ -1,7 +1,5 @@
-# backend/update_cache.py
-from utils import fetch_cves
+from utils import fetch_all_feeds
 
 if __name__ == "__main__":
-    print("Updating CVE cache...")
-    fetch_cves()
-    print("CVE cache updated successfully.")
+    fetch_all_feeds()
+    print("Cache updated successfully!")
